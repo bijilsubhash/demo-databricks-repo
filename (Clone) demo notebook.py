67eaa34e4f471b50df63e6e128477852d61d4b98
bijilsubhash <bijilsubhash>
2024-07-22 01:35:09 +0000

    adding notebook

---
+++ (Clone) demo notebook.py
@@ -0,0 +1,14 @@
+# Databricks notebook source
+print("Hello World")
+
+# COMMAND ----------
+
+# MAGIC %fs ls '/databricks-datasets'
+
+# COMMAND ----------
+
+display(dbutils.fs.ls("/databricks-datasets"))
+
+# COMMAND ----------
+
+
